e4239ae6de402ad0153952e0d5cabde996434be9
MOBUSSHAR ISLAM <mi.tonmoy4@gmail.com>
2021-01-19 17:09:27 +0600

    with comments

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
-s=input("Enter your desired string:")
-i=0
-s1=""
+s=input("Enter your input here:")           #here the input got declared and stored in a string "s"
+i=0                                         #initializing the index values of the follwoing string
+s1=""                                       #s1 will contain the output string and it's initialized with an empty string
 
-for x in s:
-    if s.index(x)==i:
-        s1+=x
-    i+=1
-print(s1)
+for x in s:                                 #using a for loop to check through each index and print it
+    if s.index(x)==i:                       #checking if the following string getting a match with the index value then it will allow it to store the value in S1
+        s1+=x                               #storing s1 whenever getting matched with the index value 
+    i+=1                                    #increasing the value of i to approach to the next index number
+print(s1)                                   #printing final value of s1 with the trimmed string
